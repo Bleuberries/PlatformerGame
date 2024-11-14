6b669765a719dee0bec200bbd2f76f2b1869e111
Mina <mylastnameiskumai@gmail.com>
2024-11-14 10:32:36 -0500

    Updated PlayerController, created GameManager

--- PlatformerGame/Assets/Scripts/PlayerController.cs
+++ PlatformerGame/Assets/Scripts/PlayerController.cs
@@ -2,37 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    //Movement Variables 
+    //Movement Variables
     Rigidbody2D rb;
     public float jumpForce;
     public float speed;
 
-    //Start is called before the first frame update
+    //Ground check
+    public bool isGrounded;
+
+
+    public GameManager gm;
+
+    // Start is called before the first frame update
     void Start()
     {
-       rb = GetComponent<Rigidbpdy2D>(); 
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector3 newPosition = transform.position;
+        Vector3 newPosition = transform.position;
 
-       //Variables to mirror player
-       Vector3 newScale = transform.localScale;
-       float currentScale = Mathf.Abs(transform.localScale.x); //take absolute value of the current x scale  
-   
-   
-     if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
-     {
-        newPosition.x -= speed;
-        newScale.x =- currentScale;
-     }
-   
-   
-   
-   
+        //variables to mirror the player
+        Vector3 newScale = transform.localScale;
+        float currentScale = Mathf.Abs(transform.localScale.x); //take absolute value of the current x scale, this is always positive
+
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition.x -= speed;
+            newScale.x = -currentScale;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition.x += speed;
+            newScale.x = currentScale;
+        }
+
+        if (Input.GetKey("w") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        transform.position = newPosition;
+        transform.localScale = newScale;
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            Debug.Log("i hit the ground");
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.tag.Equals("coin"))
+        {
+            //score goes up
+            gm.score++;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
+
+   
+
+   
+   
+   
+   
+ 
